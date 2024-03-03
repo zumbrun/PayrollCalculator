ec4977ac0ba25dedfc92177cc459b6df79ab6a92
zumbrun <zumbrun@gmail.com>
2024-03-03 16:27:18 -0600

    updated all views with no errors

--- src/calculatorform.js
+++ src/calculatorform.js
@@ -1,10 +1,34 @@
-import {rates, jobs} from './constants.js';
+import {rates, salaries} from './constants.js';
 import jsPDF from "jspdf";
-
+let userpay = {
+  "mtgs": 0,
+  "salary": 0,
+  "totalwage": 0,
+  "pera": 0,
+  "medicare": 0,
+  "net": 0,
+  "mileage": 0,
+  "phone": 0,
+  "internet": 0,
+  "misc": 0,
+  "totalpay": 0
+  };
+let userinputs = {
+  "title": "-",
+  "hours": 0,
+  "bmtgs": 0,
+  "omtgs": 0,
+  "mtgs": 0,
+  "pera": 0,
+  "phone": 0,
+  "internet": 0,
+  "miles": 0,
+  "misc":0
+}
 const submitButton = document.getElementById('submitbutton');
 const cancelButton = document.getElementById('cancelbutton');
 const printButton = document.getElementById('printbutton');
-const jobtitle = document.getElementById('job');
+const jobtitle = document.getElementById('title');
 
 submitButton.addEventListener('click', CheckInputs);
 cancelButton.addEventListener('click', Cancel);
@@ -12,211 +36,184 @@ printButton.addEventListener('click', PrintPDF);
 jobtitle.addEventListener('change', UpdateJob);
 
 function UpdateJob() {
-    let select = document.getElementById('job');
-    let option = select.options[select.selectedIndex];
-    sessionStorage.setItem(option, option.value);
-    console.log(sessionStorage.getItem(option));
-    let my_job = option.value.toLowerCase().replace(/\s/g, '');
-    
-    const div = document.querySelector("#hrs");
-    if (my_job === "supervisor") {
-      div.style.display = "block";
-    }
-    else {
-      div.style.display="hidden";
-    }
-    return my_job;
-  }
-  //operator functions on most recent 2 numbers
-  function sum (a,b) { return(a + b)};
-  function subtract (a,b) {return(a - b)};
-  function multiply (a,b) {return( a * b) };
-  function divide (a,b) { b != 0 ? a / b : "Dividing by zero is not allowed."};
+  userinputs["title"] = document.getElementById('title').value;
   
+  // get and show hours table if appropriate
+  const div = document.getElementById("hrs");
+  console.log({div});
+  console.log(userinputs.title)
+  if (userinputs.title === "supervisor") {
+    div.style.display = "block";
+  }
+  else {
+    div.style.display = "none";
+  }
+}
+//operator functions on most recent 2 numbers
+function sum (a,b) { return(a + b)};
+function subtract (a,b) {return(a - b)};
+function multiply (a,b) {return( a * b) };
+function divide (a,b) { b != 0 ? a / b : "Dividing by zero is not allowed."};
+
   //return from outputs to inputs
-  function Cancel() {
-    const inputs = document.querySelector(".inputs");
-    inputs.style.display = "block";
-    const outputs = document.querySelector(".outputs");
-    outputs.style.display = "none";
+function Cancel() {
+  const inputs = document.querySelector(".inputs");
+  inputs.style.display = "block";
+  const outputs = document.querySelector(".outputs");
+  outputs.style.display = "none";
+}
+//print payroll form
+function PrintPDF() {
+  // const outputs = document.querySelector(".outputs");
+  // outputs.style.display = "none";
+  // const printform = document.querySelector(".printform");
+  // printform.style.display = "block";
+
+  //update the pdf fields saved in session storage
+   //const bmtgs= multiply(userinputs.bmtgs, rates.meetings.rate );
+   //const omtgs= multiply(userinputs.omtgs, rates.meetings.rate);
+  let item = document.getElementById("prnttitle");
+  item.textContent = userinputs.title; 
+  item = document.getElementById("prntbmtgs");
+  item.textContent = userinputs.bmtgs; 
+  item = document.getElementById("prntomtgs");
+  item.textContent = userinputs.omtgs;
+  //update the payroll amounts
+  item = document.getElementById("sumwage");
+  item.textContent = userpay.totalwage
+  item = document.getElementById("sumpera");
+  item.textContent = userpay.pera
+  item = document.getElementById("summedicare");
+  item.textContent = userpay.medicare;
+  item = document.getElementById("sumnet");
+  item.textContent = userpay.net;
+  item = document.getElementById("summileage");
+  item.textContent = userpay.mileage;
+  item = document.getElementById("sumphone");
+  item.textContent = userpay.phone;
+  item = document.getElementById("sumint");
+  item.textContent = userpay.internet;
+  item = document.getElementById("summisc");
+  item.textContent = userpay.misc;
+  item = document.getElementById("sumtot");
+  item.textContent = userpay.totalpay;
+  //update the rates table
+  item = document.getElementById("refmeeting");
+  item.textContent = "$" + rates.meetings.rate.toString() + "/mtg"; 
+  item = document.getElementById("refmedicare");
+  item.textContent = (100*rates.medicare.rate).toFixed(2).toString() + "%"; 
+  item = document.getElementById("refpera");
+  item.textContent = (100*rates.pera.rate).toFixed(2).toString() + "%"; 
+  item = document.getElementById("refmileage");
+  item.textContent = "$" + rates.mileage.rate.toFixed(3).toString() + "/mile";
+  item = document.getElementById("refphone");
+  item.textContent = "$" + rates.phone.rate.toFixed(0).toString() + "/qtr";  
+  item = document.getElementById("refint");
+  item.textContent = "$" + rates.internet.rate.toFixed(0).toString() + "/qtr"; 
+  //update the salary table
+  item = document.getElementById("refsup");
+  item.textContent = "$" + salaries.supervisor.rate.toString() + salaries.supervisor.unit; 
+  item = document.getElementById("refclerk");
+  item.textContent = "$" + salaries.clerk.rate.toString() + salaries.clerk.unit;  
+  item = document.getElementById("refdepclerk");
+  item.textContent = "$" + salaries.deputyclerk.rate.toString() + salaries.deputyclerk.unit;  
+  item = document.getElementById("reftreas");
+  item.textContent = "$" + salaries.treasurer.rate.toString() + salaries.treasurer.unit;  
+  item = document.getElementById("refdeptreas");
+  item.textContent = "$" + salaries.deputytreasurer.rate.toString() + salaries.deputytreasurer.unit;  
+  
+  const doc = new jsPDF();
+
+  const printform = document.querySelector(".printform");
+  doc.html(printform.innerHTML, {
+    callback: function(doc) {
+      doc.save('payroll.pdf');
+    },
+    margin:[10,10,10,10],
+    autoPaging: 'text',
+    x: 0,
+    y: 0,
+    width: 190,
+    windowWidth: 675
+  });
+}
+function CheckInputs()  {
+  let field = "complete";
+  //check all inputs are completed
+  userinputs["title"] = document.getElementById("title").value;
+  userinputs["hours"] = GetValue("hours");
+  userinputs["bmtgs"] = Number(document.querySelector("input[name=bmtgs]:checked").value);
+  userinputs["omtgs"] = Number(document.querySelector("input[name=omtgs]:checked").value);
+  userinputs["phone"] = Number(document.querySelector("input[name=phone]:checked").value);
+  userinputs["internet"] = Number(document.querySelector("input[name=internet]:checked").value);
+  userinputs["miles"] = GetValue("miles");
+  userinputs["misc"] = GetValue("misc");
+  userinputs["pera"] = Number(document.querySelector("input[name=pera]:checked").value);
+
+  const userobj = Object.entries(userinputs);
+  
+  for (const [key, value] of userobj) {
+    if (value === undefined) {field=key};
+  } 
+  if (field === "complete") {
+    Calculate ();
   }
-  //print payroll form
-  function PrintPDF() {
-    
-    const doc = new jsPDF();
-  
-    // Add a title
-    doc.text("Hello, PDF World!", 10, 10);
-  
-    // Add some text
-    doc.text("This is a sample PDF generated using jsPDF.", 10, 25);
-  
-    // Add a line break
-    doc.text("", 10, 35);  // Use an empty string for a newline
-  
-    // Add a list
-    doc.text("List of items:", 10, 45);
-    doc.text("- Item 1", 20, 55);
-    doc.text("- Item 2", 20, 65);
-    doc.text("- Item 3", 20, 75);
-  
-    // Save the PDF
-    doc.save("my_first_pdf.pdf");
+  else {
+    alert(`Please complete the ${field}.`);
   }
+}
+function Calculate () {
+  // calculate outputs
+  userpay["salary"] = getSalary();
+  userpay["mtgs"] = Number(multiply(sum(userinputs.bmtgs, userinputs.omtgs),rates.meetings.rate).toFixed(2));
+  userpay["totalwage"] = Number(sum(userpay.salary, userpay.mtgs).toFixed(2));
+  userpay["pera"] = Number(multiply(userpay.totalwage, rates.pera.rate*userinputs.pera).toFixed(2));
+  userpay["medicare"] = Number(multiply(userpay.totalwage, rates.medicare.rate).toFixed(2));
+  console.log(userpay);
+  userpay["net"] = Number(subtract(userpay.totalwage, sum(userpay.pera, userpay.medicare)).toFixed(2));
+  userpay["phone"] = Number(multiply(userinputs.phone, rates.phone.rate).toFixed(2));
+  userpay["internet"] = Number(multiply(userinputs.internet, rates.internet.rate).toFixed(2));
+  userpay["mileage"] = Number(multiply(userinputs.miles, rates.mileage.rate).toFixed(2));
+  userpay["misc"] = userinputs.misc;
+  userpay["totalpay"] = userpay.net + userpay.mileage + userpay.phone + userpay.internet + userpay.misc;
+ 
+  const inputs = document.querySelector(".inputs");
+  inputs.style.display = "none";
+  const outputs = document.querySelector(".outputs");
+  outputs.style.display = "flex";
   
-  function CheckInputs()  {
-    let field = "complete";
-    //check all inputs are completed
-    const hrsSV = getValue("hours");
-    if (hrsSV===undefined){field="hours"};
-    const mtgs = getValue("meetings");
-    if (mtgs===undefined){field="meetings"};
-    const other = getValue("misc");
-    if (other===undefined){field="misc"};
-    const miles = getValue("miles");
-    if (miles===undefined){field="miles"};
-    if (!document.querySelector("input[name=pera]:checked")){field="PERA"};
-    if (!document.querySelector("input[name=internet]:checked")){field="internet"};
-    if (!document.querySelector("input[name=cellphone]:checked")){field="phone"};
-    
-    if (field === "complete") {
-      Calculate ();
+  // assign outputs
+  AssignOutputs();
+}
+
+function GetValue (myString) {
+  const input = document.getElementById(`${myString}`);
+  let amtString = input.value;
+  if (amtString.charAt(0) === "$") {amtString = amtString.substring(1)};
+  return Number(amtString);
+}
+function AssignOutputs () {
+  const entries = Object.entries(userpay);
+  for (const [key, value] of entries) {
+    const myString = "op" + key
+    const div = document.getElementById(`${myString}`);
+    if (key === "pera" || key === "medicare") {
+      div.textContent = "$ " + "(" + value + ")";
     }
     else {
-      alert(`Please complete the ${field}.`);
+      div.textContent = "$ " + value;
+    }
+  }  
+}
+const getSalary = () => {
+  let myvalue;
+  for (const [key, value] of Object.entries(salaries)) {
+    if (userinputs.title === key) {
+      myvalue = value.rate
     }
   }
-  
-  function Calculate () {
-    // set up the rates section
-    for (let key in rates) {
-      setupRates(key);
-    };
-    // determine job
-    const my_job = UpdateJob();
-    // get inputs 
-    const salary = getSalary(my_job);
-    const other = getValue("misc");
-    const miles = getValue("miles");
-    const mtgs = getValue("meetings");
-    const internet = document.querySelector("input[name=internet]:checked").value;
-    const phone = document.querySelector("input[name=cellphone]:checked").value;
-    const peraValue = document.querySelector("input[name=pera]:checked").value;
-    //console.log({salary},{other},{miles},{mtgs},{internet},{phone},{peraValue});
-    
-    // calculate intermediate outputs
-    let wage = salary;
-    if (my_job === "supervisor") {
-      const hrsSV = getValue("hours");
-      wage = multiply(hrsSV, salary);
-    }
-    const mtgAmt = multiply(mtgs, rates.meetings.rate );
-    const phoneAmt = multiply(phone, rates.phone.rate);
-    const internetAmt = multiply(internet, rates.internet.rate);
-  
-   // calculate outputs
-    const totalWage = sum(wage, mtgAmt);
-    let peraAmt = multiply(totalWage, rates.PERA.rate*peraValue);
-    peraAmt = Math.round((peraAmt + Number.EPSILON) * 100) / 100;
-    let medicareAmt = multiply(totalWage, rates.medicare.rate);
-    medicareAmt = Math.round((medicareAmt + Number.EPSILON) * 100) / 100;
-    const netAmt = subtract(totalWage, sum(peraAmt, medicareAmt));
-    let milesAmt = multiply(miles, rates.mileage.rate);
-    milesAmt = Math.round((milesAmt + Number.EPSILON) * 100) / 100;
-    const phintAmt = sum(phoneAmt, internetAmt);
-    const otherAmt = other;
-    const payment = sum(netAmt, sum(sum(milesAmt, phintAmt), otherAmt));
-    //console.log({totalWage},{peraAmt},{medicareAmt},{netAmt});
-    //console.log({milesAmt},{phintAmt}, {otherAmt});
-  
-    const inputs = document.querySelector(".inputs");
-    inputs.style.display = "none";
-    const outputs = document.querySelector(".outputs");
-    outputs.style.display = "flex";
-    
-    // assign outputs
-    putValue("tw", totalWage);
-    putValue("peraAmt",peraAmt);
-    putValue("medicareAmt", medicareAmt);
-    putValue("net", netAmt);
-    putValue("milesAmt", milesAmt);
-    putValue("phintAmt", phintAmt);
-    putValue("otherAmt", otherAmt);
-    putValue("payment",payment);
+  if (userinputs.title === "supervisor") {
+    myvalue = multiply(userinputs.hours, value);
   }
-  function setupRates(key) {
-    const div = document.getElementById(key);
-    let msg = key;
-    switch(msg) {
-        case "PERA":
-            const PERApercent = rates.PERA.rate * 100.0;
-            msg = `PERA ${PERApercent}%`;
-            break;
-        case "medicare":
-            const medicarePercent = (rates.medicare.rate * 100.0).toFixed(2);
-            msg = `medc ${medicarePercent}%`;
-            break;
-        case "mileage":
-            msg = `\$${rates.mileage.rate}\/mi`;
-            break;
-        case "meetings":
-            msg = `\$${rates.meetings.rate}\/mtg  `;
-            break;
-        case "internet":
-            msg = `int \$${rates.internet.rate}\/qtr`;
-            break;
-        case "phone":
-            msg = `ph \$${rates.phone.rate}\/qtr`;
-            break;
-        default:
-            msg = 'uhoh';
-    };
-    div.textContent = msg;   
-  };
-  const getSalary = (my_job) => {
-    for (const [key, value] of Object.entries(jobs)) {
-      if (my_job === key) {
-        return value.rate;
-      }
-    }
-  };
-  function getValue (myString) {
-    const input = document.getElementById(`${myString}`);
-    let amtString = input.value;
-    if (amtString.charAt(0) === "$") {amtString = amtString.substring(1)};
-    return Number(amtString);
-  }
-  function putValue (myString, amt) {
-    const div = document.getElementById(`${myString}`)
-    div.textContent = "$" + Number(amt).toFixed(2);
-    sessionStorage.setItem(myString, div.textContent );
-    console.log(sessionStorage.getItem(myString));
-  }
-  function inputFormat(id, string) {
-        switch (id) {
-        case "hrsSV":
-            return string.length === 0 ? 0 : string;
-        case "miles":
-            return string.length === 0 ? 0 : string;
-        case "mtgs":
-            return string.length === 0 ? 0 : string;
-        case "ph":
-            return string.length === 0 ? 1 : string;
-        case "int":
-            return string.length === 0 ? 1 : string;
-        case "other":
-            if (string.length === 0) {return "$0.00"}
-            if (string === "$") {return "$0.00"}
-            return currencyFormat(string);
-        case "salary":
-            if (string.length === 0) {return "$0.00"}
-            if (string === "$") {return "$0.00"}
-            return currencyFormat(string);
-    };
-  }
-  function currencyFormat(string) {
-    splitString = string.split("$").join("");
-    result = Number(splitString).toLocaleString('en-US', {style: 'currency', currency:'USD'});
-    return result;
-  }
-  
+  return myvalue;
+}; 
--- src/constants.js
+++ src/constants.js
@@ -1,17 +1,17 @@
 const rates = {
-    PERA: {unit: "%", rate:0.05},
+    pera: {unit: "%", rate:0.05},
     medicare: {unit: "%", rate:0.0145},
-    meetings: {unit: "$/mtg", rate:100},
-    mileage: {unit: "$/mile", rate: 0.625},
-    phone: {unit: "$/qtr", rate: 30},
-    internet: {unit: "$/qtr", rate: 30},
+    meetings: {unit: "/mtg", rate:100},
+    mileage: {unit: "/mile", rate: 0.670},
+    phone: {unit: "/qtr", rate: 30},
+    internet: {unit: "/qtr", rate: 30},
 };
-const jobs = {
-    supervisor: {rate: 30},
-    clerk: {rate: 750},
-    deputyclerk: {rate: 150},
-    treasurer: {rate: 550},
-    deputytreasurer: {rate: 0},
+const salaries = {
+    supervisor: {unit:"/hr", rate: 30},
+    clerk: {unit:"/qtr", rate: 750},
+    deputyclerk: {unit:"/qtr", rate: 150},
+    treasurer: {unit:"/qtr", rate: 550},
+    deputytreasurer: {unit:"/qtr", rate: 0},
 };
 
-export {rates, jobs};
+export {rates, salaries};
